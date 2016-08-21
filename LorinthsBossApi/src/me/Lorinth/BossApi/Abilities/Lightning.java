003785a8ef6e982c7109f981a9490b2cc1149966
Kyle Wolfgang Kattrein <kattreinkyle@gmail.com>
2016-08-20 20:09:28 -0400

    Ugh, making lightning not sound effect thru the whole world

--- LorinthsBossApi/src/me/Lorinth/BossApi/Abilities/Lightning.java
+++ LorinthsBossApi/src/me/Lorinth/BossApi/Abilities/Lightning.java
@@ -1,5 +1,6 @@
 package me.Lorinth.BossApi.Abilities;
 
+import org.bukkit.Sound;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -25,20 +26,23 @@ public class Lightning extends Action{
 	public void execute(Creature ent){
 		if(type == TargetType.Target){
 			LivingEntity targ = ent.getTarget();
-			targ.getWorld().strikeLightningEffect(targ.getLocation());
+			targ.getWorld().strikeLightningEffect(targ.getLocation()).setSilent(true);
+            targ.getWorld().playSound(targ.getLocation(), Sound.ENTITY_LIGHTNING_IMPACT, 1, 1);
 			if(damage > 0){
 				targ.damage(damage);
 			}
 		}
 		else if(type == TargetType.Self){
-			ent.getWorld().strikeLightningEffect(ent.getLocation());
+			ent.getWorld().strikeLightningEffect(ent.getLocation()).setSilent(true);
+            ent.getWorld().playSound(ent.getLocation(), Sound.ENTITY_LIGHTNING_IMPACT, 1, 1);
 			if(damage > 0){
 				ent.damage(damage);
 			}
 		}
 		else if(type == TargetType.AoePlayers){
 			for(Player p : getNearbyPlayers(radius, ent)){
-				p.getWorld().strikeLightningEffect(p.getLocation());
+				p.getWorld().strikeLightningEffect(p.getLocation()).setSilent(true);
+                p.getWorld().playSound(p.getLocation(), Sound.ENTITY_LIGHTNING_IMPACT, 1, 1);
 				if(damage > 0){
 					p.damage(damage);
 				}
@@ -46,7 +50,8 @@ public class Lightning extends Action{
 		}
 		else if(type == TargetType.AoeCreatures){
 			for(Creature c : getNearbyCreatures(radius, ent)){
-				c.getWorld().strikeLightningEffect(c.getLocation());
+				c.getWorld().strikeLightningEffect(c.getLocation()).setSilent(true);
+                c.getWorld().playSound(c.getLocation(), Sound.ENTITY_LIGHTNING_IMPACT, 1, 1);
 				if(damage > 0){
 					c.damage(damage);
 				}
